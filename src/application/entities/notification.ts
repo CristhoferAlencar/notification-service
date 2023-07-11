6ea8d7ab5b0cc99ff40b870b8a62f3531ba2633b
Cristhofer Alencar <cristhofer-alencar@hotmail.com>
2023-07-11 20:13:09 -0300

    Created notification and content tests

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 
-import { Content } from './content';
+import { Replace } from "src/helpers/Replace";
+import { Content } from "./content";
 
 export interface NotificationProps {
   recipientId: string;
@@ -13,8 +14,11 @@ export interface NotificationProps {
 export class Notification {
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-    this.props = props;
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
   }
 
   public set recipientId(recipientId: string) {
